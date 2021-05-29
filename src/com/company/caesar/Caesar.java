b3f7e6a7d4dbb7cdd7eb59cac212bcc7384c039c
Serah wakaba <serahwakaba9@gmail.com>
2021-05-29 09:58:28 +0300

    make number be between 0 and 26 commit

--- src/com/company/caesar/Caesar.java
+++ src/com/company/caesar/Caesar.java
@@ -2,6 +2,15 @@ package com.company.caesar;
 
 public class Caesar {
 
+    public static String encrypt(String plainText, int shift){
+        if(shift>26){
+            shift = shift%26;
+        }
+        else (shift<0){
+            shift= (shift%26)+26;
+        }
+    }
+
 
 
     public static void main(String[] args) {
