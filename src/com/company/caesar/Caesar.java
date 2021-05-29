e8d50ae4145188b0b6a121be418782173267edcc
Serah wakaba <serahwakaba9@gmail.com>
2021-05-29 10:51:44 +0300

    decryption method commit

--- src/com/company/caesar/Caesar.java
+++ src/com/company/caesar/Caesar.java
@@ -41,6 +41,45 @@ public class Caesar {
     }
 
 
+    public static String dencrypt(String plainText, int shift){
+        if(shift>26){
+            shift = shift%26;
+        }else if (shift< 0 ) {
+            shift = (shift % 26) + 26;
+        }
+        String cipherText = "";
+        int length = plainText.length();
+        int i;
+        for (i = 0; i<length; i++){
+            char ch = plainText.charAt(i);
+            if (Character.isLetter(ch)){
+                if(Character.isLowerCase(ch)){
+                    char c = (char) (ch - shift);
+                    if (c < 'a') {
+                        cipherText += (char) (ch + (26 - shift));
+                    } else {
+                        cipherText += c;
+                    }
+
+                }else if(Character.isUpperCase(ch)){
+                    char c = (char) (ch - shift);
+                    if( c >'A'){
+                        cipherText += (char)(ch + (26-shift));
+                    }else {
+                        cipherText += c;
+                    }
+
+                }
+
+            }else {
+                cipherText += ch;
+            }
+        }
+
+        return cipherText;
+    }
+
+
 
     public static void main(String[] args) {
         String text = "This is some message";
